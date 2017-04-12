1bdb8e9efb0022830f28c11250267fa782149884
Bruno Parreira Lopes <brunoparreiralopes@gmail.com>
2017-04-12 17:49:36 +0000

    Release 1.0.0.1

--- src/js/map.js
+++ src/js/map.js
@@ -1,12 +1,165 @@
 function initMap() {
+ var styledMapType = new google.maps.StyledMapType(
+  [{
+   elementType: 'geometry',
+   stylers: [{
+    color: '#ebe3cd'
+   }]
+  }, {
+   elementType: 'labels.text.fill',
+   stylers: [{
+    color: '#523735'
+   }]
+  }, {
+   elementType: 'labels.text.stroke',
+   stylers: [{
+    color: '#f5f1e6'
+   }]
+  }, {
+   featureType: 'administrative',
+   elementType: 'geometry.stroke',
+   stylers: [{
+    color: '#c9b2a6'
+   }]
+  }, {
+   featureType: 'administrative.land_parcel',
+   elementType: 'geometry.stroke',
+   stylers: [{
+    color: '#dcd2be'
+   }]
+  }, {
+   featureType: 'administrative.land_parcel',
+   elementType: 'labels.text.fill',
+   stylers: [{
+    color: '#ae9e90'
+   }]
+  }, {
+   featureType: 'landscape.natural',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#dfd2ae'
+   }]
+  }, {
+   featureType: 'poi',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#dfd2ae'
+   }]
+  }, {
+   featureType: 'poi',
+   elementType: 'labels.text.fill',
+   stylers: [{
+    color: '#93817c'
+   }]
+  }, {
+   featureType: 'poi.park',
+   elementType: 'geometry.fill',
+   stylers: [{
+    color: '#a5b076'
+   }]
+  }, {
+   featureType: 'poi.park',
+   elementType: 'labels.text.fill',
+   stylers: [{
+    color: '#447530'
+   }]
+  }, {
+   featureType: 'road',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#f5f1e6'
+   }]
+  }, {
+   featureType: 'road.arterial',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#fdfcf8'
+   }]
+  }, {
+   featureType: 'road.highway',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#f8c967'
+   }]
+  }, {
+   featureType: 'road.highway',
+   elementType: 'geometry.stroke',
+   stylers: [{
+    color: '#e9bc62'
+   }]
+  }, {
+   featureType: 'road.highway.controlled_access',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#e98d58'
+   }]
+  }, {
+   featureType: 'road.highway.controlled_access',
+   elementType: 'geometry.stroke',
+   stylers: [{
+    color: '#db8555'
+   }]
+  }, {
+   featureType: 'road.local',
+   elementType: 'labels.text.fill',
+   stylers: [{
+    color: '#806b63'
+   }]
+  }, {
+   featureType: 'transit.line',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#dfd2ae'
+   }]
+  }, {
+   featureType: 'transit.line',
+   elementType: 'labels.text.fill',
+   stylers: [{
+    color: '#8f7d77'
+   }]
+  }, {
+   featureType: 'transit.line',
+   elementType: 'labels.text.stroke',
+   stylers: [{
+    color: '#ebe3cd'
+   }]
+  }, {
+   featureType: 'transit.station',
+   elementType: 'geometry',
+   stylers: [{
+    color: '#dfd2ae'
+   }]
+  }, {
+   featureType: 'water',
+   elementType: 'geometry.fill',
+   stylers: [{
+    color: '#b9d3c2'
+   }]
+  }, {
+   featureType: 'water',
+   elementType: 'labels.text.fill',
+   stylers: [{
+    color: '#92998d'
+   }]
+  }], {
+   name: 'Styled Map'
+  });
+
  var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 5,
   center: {
    lat: -33.9,
-   lng: 151.2
+   lng: 151.2,
+   mapTypeControlOptions: {
+    mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+     'styled_map'
+    ]
+   }
   }
  });
 
+ map.mapTypes.set('styled_map', styledMapType);
+ map.setMapTypeId('styled_map');
  setMarkers(map);
 }
 
@@ -32,22 +185,22 @@ var dangerzones = [
 function setMarkers(map) {
  // Adds markers to the map.
 
-var imagesf = new google.maps.MarkerImage(
-    "./src/img/safezone.png",
-    null, /* size is determined at runtime */
-    null, /* origin is 0,0 */
-    null, /* anchor is bottom center of the scaled image */
-    new google.maps.Size(54, 54)
-); 
+ var imagesf = new google.maps.MarkerImage(
+  "./src/img/safezone.png",
+  null, /* size is determined at runtime */
+  null, /* origin is 0,0 */
+  null, /* anchor is bottom center of the scaled image */
+  new google.maps.Size(54, 54)
+ );
 
 
-var imagedg = new google.maps.MarkerImage(
-    "./src/img/dangerzone.png",
-    null, /* size is determined at runtime */
-    null, /* origin is 0,0 */
-    null, /* anchor is bottom center of the scaled image */
-    new google.maps.Size(27, 27)
-);  
+ var imagedg = new google.maps.MarkerImage(
+  "./src/img/dangerzone.png",
+  null, /* size is determined at runtime */
+  null, /* origin is 0,0 */
+  null, /* anchor is bottom center of the scaled image */
+  new google.maps.Size(27, 27)
+ );
 
 
  for (var i = 0; i < safezones.length; i++) {
@@ -64,8 +217,8 @@ var imagedg = new google.maps.MarkerImage(
    zIndex: sf[3]
   });
  }
- 
-  for (var i = 0; i < dangerzones.length; i++) {
+
+ for (var i = 0; i < dangerzones.length; i++) {
   var dg = dangerzones[i];
   var marker2 = new google.maps.Marker({
    position: {
@@ -79,5 +232,5 @@ var imagedg = new google.maps.MarkerImage(
    zIndex: dg[3]
   });
  }
- 
+
 }
